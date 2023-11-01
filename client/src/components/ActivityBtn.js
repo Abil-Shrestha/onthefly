4b0ee64ca280606f102a6f91effff46af8ec738a
csi-Abil <abil.shrestha@cix.csi.cuny.edu>
2023-10-31 22:34:58 -0400

    vote on btn

--- client/src/components/ActivityBtn.js
+++ client/src/components/ActivityBtn.js
@@ -6,7 +6,15 @@ const ActivityBtn = (props) =>  {
   const [num_votes, setNumVotes] = useState(props.num_votes)
 
   const updateCount = () => {
-
+    const options = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify(num_votes + 1)
+    }
+    fetch(`http://localhost:3000/api/activities${props.id}`,options)
+    setNumVotes((num_votes) => num_votes +1)
   }
 
   return (
