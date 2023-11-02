5398b798ace3a56cec552ac22698f5039c09fc08
csi-Abil <abil.shrestha@cix.csi.cuny.edu>
2023-11-01 23:51:56 -0400

    auth

--- server/server.js
+++ server/server.js
@@ -4,13 +4,37 @@ import tripRoutes from './routes/trips.js'
 import activityRoutes from './routes/activities.js'
 import destinationRoutes from './routes/destination.js'
 import tripDestinationRoutes from './routes/trip_dest.js'
+import passport from 'passport'
+import session from 'express-session'
+import { GitHub } from './config/auth.js'
+import authRoutes from './routes/auth.js'
 
 const app  = express()
+app.use(session({
+  secret: 'codepath',
+  resave: false,
+  saveUninitialized: true
+}))
 
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:3001',
+  methods: 'GET,POST,PUT,DELETE,PATCH',
+  credentials: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(GitHub)
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+
+passport.deserializeUser((user, done) => {
+  done(null, user)
+})
 
 app.get('/', (req, res) => {
     res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">✈️ Wohooo</h1>')
@@ -20,6 +44,7 @@ app.use('/api/trips', tripRoutes)
 app.use('/api/activities', activityRoutes)
 app.use('/api/destinations', destinationRoutes)
 app.use('/api/trips-destinations', tripDestinationRoutes)
+app.use('/auth', authRoutes)
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
